9cf6bae8ea0f9f0a2788c2903706454925bc9325
Rachit Gupta <rachitgupta0604@gmail.com>
2025-06-04 14:14:42 +0530

    Practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import Greet from "./components/Greet";
+import Add from "./components/Add";
+import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
 const App = () => {
   return <div>
     <Greet />
+    <Add />
+    <WelcomeMessage/>
+    <Header/>
+    <MainContent/>
+    <Footer/>
   </div>;
 };
 export default App;
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <maincontent>
+        <h2>Main Content</h2>
+        <p>Burrrrhhhhh</p>
+    </maincontent>
+  );
+}
+export default MainContent; 
