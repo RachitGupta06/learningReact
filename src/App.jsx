5b17ab2eec761eb6a5b3437644d194b6fb2a22a1
Rachit Gupta <rachitgupta0604@gmail.com>
2025-06-09 00:06:43 +0530

    Practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import { FaCartPlus } from "react-icons/fa6";
 
 import Style from "./components/Style";
+import ProfileCard from "./components/ProfileCard";
+import IconComponent from "./components/IconComponent";
+const handleCheck = () =>console.log(Math.round(Math.random()*10));
+const Botton =()=>{
+  return( 
+    <button onClick={handleCheck}>Click</button>
+  )
+}
 const App = () => {
-  return <div>
+  return( <div>
     <Style/>
+    <ProfileCard/>
+    <IconComponent/>
+    <Botton/>
   </div>
-};
+  )
+}
 export default App;
---
+++ src/components/ProfileCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const ProfileCard = () => {
+    const styling = {
+        backgroundColor: 'lightgray',
+        padding : '15px',
+        borderRadius : '8px',
+        color : 'black',
+    };
+  return (
+    <div style={styling}>
+        <h1>Hello World</h1>
+        <p>mine ikjkd ihwiuhiwh iohwiugigw uwiwugiww</p>
+    </div>
+  )
+}
+
+export default ProfileCard
